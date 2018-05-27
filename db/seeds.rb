a00181db6330d88c89f7f7b072c7aae9b2ba4a1d
Angel Melendez <iquit@osx-iquit.localdomain>
2018-05-27 14:25:31 -0400

    loadedphotos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+my_products = [{name: ""}, {}, {}]
+
+
+product = Product.create([
+  {name: "Rainest Boot", description: "Good for rainy days", catagory: "woman", price_in_cents: 1000, image_url: "https://media.hunterboots.com/public/api/cloudfront/image/25181/1200/0.8/90/FFF/0/20170712164456ORG_BLK_AW17_WFT1000RMA_product.jpg"}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  {name: "", description: "", catagory: "woman", price_in_cents: 1000, image_url: ""}
+  ])
